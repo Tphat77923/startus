9b380bde1d9cbc5791444770bcf073dc4a92a264
Trần Tấn <>
2022-11-26 14:14:17 +0000

    edit official

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const client = new Client();
 
 
 client.on('ready', () => {
-    console.log('I am ready!');
-
+    console.log(`I am login in ${client.user.tag}`);
+})
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
@@ -15,10 +15,12 @@ client.on('message', message => {
 });
 
 client.on("message", (message) => {
-	if(message.content === "s.starts"){
+	if(message.content === "s.tpstart"){
 setInterval(function(){
-	
+	let log = "1045966681253613679"
+							const logch = client.channels.cache.get(log);
 message.channel.send("tp!modstarts");
+	
 	const collector = message.channel.createMessageCollector((m) => m.author.id === "923841520191283231" && ['PONG'].includes(m.content), {
             time: 2000,
             max: 1
@@ -27,9 +29,8 @@ message.channel.send("tp!modstarts");
             const confirm = m.content === 'PONG';
             collector.stop();
             if (confirm) {
-							let log = "1045966681253613679"
-							const logch = client.channels.cache.get(log);
-											logch.send('online! :) ');
+							
+											logch.send('✅ the bot is online :) ');
 						}
 				});
 					
@@ -38,7 +39,7 @@ message.channel.send("tp!modstarts");
                 return logch.send(':x: bot is offline :( ');
 
     })
-},3000);
+},300000);
 
 
 	}
